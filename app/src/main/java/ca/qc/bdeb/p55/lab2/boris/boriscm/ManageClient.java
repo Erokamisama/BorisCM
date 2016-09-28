ea4f487e4e03919a4783b367722a62f07638e3cb
Erokamisama <cedric.wutchanki1991@gmail.com>
2016-09-28 02:21:07 -0400

    InitialCommit

---
+++ app/src/main/java/ca/qc/bdeb/p55/lab2/boris/boriscm/ManageClient.java
@@ -0,0 +1,13 @@
+package ca.qc.bdeb.p55.lab2.boris.boriscm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManageClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_client);
+    }
+}
